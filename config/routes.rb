08cdd209c063790a63123fe355dd844a6436fc29
GitaristErik <Gitarist.Erik@gmail.com>
2020-12-20 18:31:41 +0200

    Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 root 'posts#index', as: 'home'
 
 get 'about' => 'pages#about', as: 'about'
+get 'persons/profile', as: 'user_root'
 
 resources :posts do
   resources :comments
